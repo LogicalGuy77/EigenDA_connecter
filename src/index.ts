39d974b37e9eb8057be0487fb1d002f68bdf6af0
LogicalGuy77 <harshitacademia@gmail.com>
2025-02-09 01:38:20 +0530

    api created for eigen layer EigenDA

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+//@ts-nocheck
+import express from "express";
+import cors from "cors";
+import { storeBlob } from "./store_blob";
+import { retrieveBlob } from "./retrieve_blob";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Parse JSON bodies
+
+// Post request route
+app.post("/api/store", async (req, res) => {
+  try {
+    const data = await req.body.message;
+    let obj = await storeBlob(data);
+    res.status(200).json({
+      success: true,
+      message: "Data received successfully",
+      data: obj,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error processing request",
+      error: error?.message,
+    });
+  }
+});
+
+app.post("/api/retrive", async (req, res) => {
+  try {
+    const { headerhash, blobindex } = await req.body;
+    let obj = await retrieveBlob(headerhash, blobindex);
+    res.status(200).json({
+      success: true,
+      message: "Data received successfully",
+      data: obj,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error processing request",
+      error: error?.message,
+    });
+  }
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
